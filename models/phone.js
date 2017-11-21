994be21b4ed5e9a60f7f84e2818ef5878031d847
fish444556 <jialinl@mtu.edu>
2017-11-21 09:35:12 -0500

    saved phone detail to mongodb

---
+++ models/phone.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var phoneSchema = mongoose.Schema({
+  additionalFeatures: String,
+  android: Object,
+  availability: Object,
+  battery: Object,
+  camera: Object,
+  connectivity: Object,
+  description: String,
+  display: Object,
+  hardware: Object,
+  id: String,
+  images: Object,
+  name: String,
+  sizeAndWeight: Object,
+  storage: Object
+});
+
+var phoneModel = mongoose.model('phones', phoneSchema);
+module.exports = phoneModel;
